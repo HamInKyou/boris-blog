2bdd9066dd58170d1e6e4159b38e9ac6d37d93e3
HamInKyou <hfire1207@gmail.com>
2024-04-22 20:55:07 +0900

    [실습2] 로딩 최적화 - 텍스트 압축

--- server/server.js
+++ server/server.js
@@ -2,13 +2,9 @@ const express = require('express');
 const app = express();
 const port = 5001;
 const path = require('path');
+const compression = require('compression');
 
 const header = {
-  /*
-   * TODO 2.
-   * [로딩 최적화 - 텍스트 압축] 
-   * 텍스트 파일들이 압축되지 않은 상태로 전송되고 있습니다.
-   */
 
   /*
    * TODO 3.
@@ -23,6 +19,7 @@ const header = {
   },
 }
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, '../build'), header));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
